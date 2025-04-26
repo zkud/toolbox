ffa32b791c1c27dba6077a7d6c9d908bfe1bc284
zkud <anton.kudyrka@gmail.com>
2025-04-26 11:16:33 +0300

    fix: fixed wrong path to nvim .config

--- nvim/base/run.sh
+++ nvim/base/run.sh
@@ -4,7 +4,7 @@
 
 docker run --volume $(pwd):/app \
   --user $NVIM_BASE_USER_NAME \
-  --volume ~/.toolbox/nvim/base/nvim-state/.config/nvim:/home/$USER/.config/nvim \
+  --volume ~/.toolbox/nvim/base/nvim-state/.config/nvim:/home/$NVIM_BASE_USER_NAME/.config/nvim \
   -w /app \
   --rm \
   -it $NVIM_BASE_IMAGE_NAME sh -uelic "nvim $@"
--- nvim/rust/run.sh
+++ nvim/rust/run.sh
@@ -4,7 +4,7 @@
 
 docker run --volume $(pwd):/app \
   --user $NVIM_BASE_USER_NAME \
-  --volume ~/.toolbox/nvim/rust/nvim-state/.config/nvim:/home/$USER/.config/nvim \
+  --volume ~/.toolbox/nvim/rust/nvim-state/.config/nvim:/home/$NVIM_BASE_USER_NAME/.config/nvim \
   -w /app \
   --rm \
   -it $NVIM_RUST_IMAGE_NAME sh -uelic "nvim $@"
