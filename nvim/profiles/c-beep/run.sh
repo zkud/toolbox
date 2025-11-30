7473ff81f2c2fd347901e0fba2ba089e6bb248b9
zkud <anton.kudyrka@gmail.com>
2025-11-30 14:00:28 +0300

    fix: fix missing ffmpeg in c-beep

--- nvim/profiles/c-beep/run.sh
+++ nvim/profiles/c-beep/run.sh
@@ -7,4 +7,4 @@ docker run --volume $(pwd):/app \
   --volume ~/.toolbox/nvim/profiles/c-beep/home:/home/$NVIM_BASE_USER_NAME \
   -w /app \
   --rm \
-  -it $NVIM_C_IMAGE_NAME sh -uelic "nvim $@"
+  -it $NVIM_C_BEEP_IMAGE_NAME sh -uelic "nvim $@"
