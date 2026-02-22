e09f512a918f822efefdc6b1d1061034fca1141f
zkud <anton.kudyrka@gmail.com>
2026-02-22 22:38:28 +0300

    fix: add workdir and install additional deps

--- opencode/run.sh
+++ opencode/run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-docker run -it -u node --volume $(pwd):/app opencode sh -uelic "source ~/.profile; opencode -m ollama/nemotron-3-nano"
+docker run -it \
+	-u node \
+	--volume $(pwd):/app \
+	-w /app \
+	opencode sh -uelic "source ~/.profile; opencode -m ollama/nemotron-3-nano"
 
